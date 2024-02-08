cb478a27f614bb59d0a93934b476d53279dd48e1
ameya-6964 <belvalkar7@gmail.com>
2024-02-08 15:21:27 +0530

    Created Boiler Plate Of JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
+    private static final String url="jdbc:mysql://localhost:3306/?user=root";
+    private static final String username="root";
+    private static final String password="root";
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement statement = connection.createStatement();
+
+        }
+         catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
