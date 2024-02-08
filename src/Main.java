be164fd7ae5d267494bf725863f72119d657b932
ameya-6964 <belvalkar7@gmail.com>
2024-02-08 15:35:10 +0530

    Performed Read Operation Through JDBC

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
-    private static final String url="jdbc:mysql://localhost:3306/?user=root";
+    private static final String url="jdbc:mysql://localhost:3306/mydb";
     private static final String username="root";
     private static final String password="root";
     public static void main(String[] args) {
@@ -16,7 +13,18 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(url,username,password);
             Statement statement = connection.createStatement();
-
+            String query = " select * from students";
+            ResultSet resultSet =  statement.executeQuery(query);
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println("ID: " + id);
+                System.out.println("NAME: " + name);
+                System.out.println("AGE: " + age);
+                System.out.println("MARKS: " + marks);
+            }
         }
          catch (SQLException e) {
             System.out.println(e.getMessage());
